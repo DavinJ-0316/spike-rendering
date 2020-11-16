d66db9c9736b3e6415fb5746752f8d068b82e12e
Long Zhao <zlong@outlook.com>
2020-11-16 15:41:31 +1100

    ✨ Introduce getProduct API

--- apis/getProduct.js
+++ apis/getProduct.js
@@ -1,23 +1,5 @@
-import { Octokit } from '@octokit/core';
+import axios from 'axios';
 
-const octokit = new Octokit({ auth: 'ee6245315beb7830b91e61d1ce94e47428ad8563' });
-
-const decode = (str) => {
-  try {
-    return btoa(str)
-  } catch(e) {
-    return Buffer.from(str, 'base64').toString();
-  }
-};
-
-const getProduct = ()  => octokit
-  .request('GET /repos/{owner}/{repo}/contents/{path}', {
-    owner: 'KieraDOG',
-    repo: 'spike-rendering-content',
-    path: 'product.json'
-  })
-  .then(({ data }) => data.content)
-  .then((content) => decode(content))
-  .then((decoded) => JSON.parse(decoded));
+const getProduct = ()  => axios.get('/api/product').then(({ data }) => data);
 
 export default getProduct;
---
+++ pages/api/product.js
@@ -0,0 +1,23 @@
+import { Octokit } from '@octokit/core';
+
+const octokit = new Octokit({ auth: process.env.GIT_HUB_TOKEN });
+
+const decode = (str) => {
+  try {
+    return btoa(str)
+  } catch(e) {
+    return Buffer.from(str, 'base64').toString();
+  }
+};
+
+const getProduct = (req, res)  => octokit
+  .request('GET /repos/{owner}/{repo}/contents/{path}', {
+    owner: 'KieraDOG',
+    repo: 'spike-rendering-content',
+    path: 'product.json'
+  })
+  .then(({ data }) => data.content)
+  .then((content) => decode(content))
+  .then((decoded) => res.status(200).json(decoded));
+
+export default getProduct;
